c713afb51b061222d2e30202dcbb04ae46c2ec0e
Bukola Eunice <thelmabukky87@gmail.com>
2022-10-14 21:55:35 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Travel());
+}
+
+class Travel extends StatelessWidget {
+  const Travel({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
